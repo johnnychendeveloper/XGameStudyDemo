3e62609f24ea34d2c8b3aee20a14a67ad900f654
johnnychendeveloper <johnnychendeveloper@gmail.com>
2016-12-03 01:51:11 -0500

    add test code to test NSTimer

---
+++ XGameStudyDemo/TestClasses/OtherViewController.h
@@ -0,0 +1,13 @@
+//
+//  OtherViewController.h
+//  XGameStudyDemo
+//
+//  Created by JohnnyChen on 16/12/2.
+//  Copyright © 2016年 YY.Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OtherViewController : UIViewController
+
+@end
